f2166d127ddc1f62ccfef2d85b19364cf28a9476
Sergii Grebeniuk <sergii.g@preply.com>
2018-10-19 15:31:43 +0200

    Externals

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
-const path = require("path");
-const baseConfig = require("./webpack.config.base");
+const path = require('path');
+const baseConfig = require('./webpack.config.base');
 
 module.exports = {
 	...baseConfig,
-	entry: path.join(__dirname, "src/index.js"),
+	entry: path.join(__dirname, 'src/index.js'),
 	output: {
-		libraryTarget: "commonjs2"
+		libraryTarget: 'commonjs2',
 	},
-	// externals: {
-	// 	react: "commonjs react"
-	// }
+	externals: {
+		react: 'commonjs react',
+		'react-dom': 'commonjs react-dom',
+		'react-native': 'commonjs react-native',
+		'react-art': 'commonjs react-art',
+	},
 };
